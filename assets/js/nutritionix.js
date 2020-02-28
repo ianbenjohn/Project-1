b770761e2fd502433fe6c818b401a33c93197c1d
Max Lemieux <max.lemieux@gmail.com>
2020-02-27 19:55:51 -0800

    minimum example for connection to nutritionix

--- assets/js/nutritionix.js
+++ assets/js/nutritionix.js
@@ -3,17 +3,31 @@ const getNutrition = (ingredients) => {
     const queryUrl = 'https://trackapi.nutritionix.com/v2/natural/nutrients';
     const NutritionixAppID = '3cff7c33';
     const NutritionixAppKey = 'bc57bf3815504c053f18cfd02f2518c9';
-    const headerData = `Content-Type:application/json, x-app-id:${NutritionixAppID}, x-app-key:${NutritionixAppKey}`;
+    const headerData = 
+        {
+            'Content-Type': 'application/json', 
+            'accept': 'application/json',
+            'x-app-id': NutritionixAppID, 
+            'x-app-key': NutritionixAppKey,
+            'x-remote-user-id': '0',
+            'cache-control': 'no-cache',
+        };
     for (ingredient of ingredients) {
-        ingredientStory += `${ingredient.quantity} ${ingredient.ingredient},`
+        ingredientStory += `${ingredient.quantity} ${ingredient.ingredient} and `
     };
     $.ajax({
-        url: queryUrl,
-        headers: headerData,
-        request: 'GET',
-        data:
-            {
-                query: ingredientStory,
-            },
-    }).then(console.log(response));
+            async: true,
+            crossDomain: true,
+            url: queryUrl,
+            method: 'POST',
+            headers: headerData,
+            data: JSON.stringify(
+                {
+                    "query": ingredientStory,
+                }
+            ),
+        })
+    .done(function(response) {
+        console.log(response);
+    });
 };
