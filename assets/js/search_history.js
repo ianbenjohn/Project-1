3c45ae66adf8d874fc753db90b99287af1d4b093
Max Lemieux <max.lemieux@gmail.com>
2020-02-29 12:42:01 -0800

    search history shows on page if was initially empty, will not process empty history

--- assets/js/search_history.js
+++ assets/js/search_history.js
@@ -3,26 +3,35 @@ Works with localStorage to keep the history list updated on new page loads
 Also connects to search button and form submit
 Runs on page load and on search button click
 */
-
 const displaySearchHistory = (newSearchTerm) => {
-    const searchHistoryList = $('#search-history-list');
     let searchHistory = [];
     if (localStorage.getItem('search_history')) {
         searchHistory = JSON.parse(localStorage.getItem('search_history'));
-        $('#search-history').removeClass('hidden');
     };
 
+    /* Check if search history already includes this term */
     if (searchHistory.includes(newSearchTerm)) {
         return;
     } else if (newSearchTerm != null) {
+        /* If there is a new term, add it to the search history */
         searchHistory.push(newSearchTerm);
         localStorage.setItem('search_history', JSON.stringify(searchHistory));
     };
 
+    /* If the search history is not empty, show it on page */
+    if (searchHistory.length > 0) {
+        updateSearchHistoryList(searchHistory);
+        $('#search-history').removeClass('hidden');
+    }
+};
+
+/* Update the element on page with list of search history */
+const updateSearchHistoryList = (searchHistory) => {
+    const searchHistoryList = $('#search-history-list');
     searchHistoryList.empty();
     for (let i=0; i<searchHistory.length; i++) {
         const searchTermElement = $('<li>');
         searchTermElement.text(searchHistory[i]);
         searchHistoryList.append(searchTermElement);
-    };
-}
+    };    
+};
