a0eb7682378dace946fea31fae98357ea77d0889
Max Lemieux <max.lemieux@gmail.com>
2020-02-29 11:27:53 -0800

    added main.js for binding event listeners to default page elements

--- assets/js/search_history.js
+++ assets/js/search_history.js
@@ -1,4 +1,25 @@
 /* Search history function
 Works with localStorage to keep the history list updated on new page loads
 Also connects to search button and form submit
+Runs on page load and on search button click
 */
+
+const displaySearchHistory = (newSearchTerm) => {
+    const searchHistoryList = $('#search-history-list');
+    let searchHistory = [];
+    if (localStorage.getItem('search_history')) {
+        searchHistory = localStorage.getItem('search_history');
+    };
+
+    if (searchHistory.includes(newSearchTerm) {
+        return;
+    } else {
+        searchHistory.push(newSearchTerm);
+        localStorage.setItem('search_history', searchHistory);
+    };
+
+    for (searchTerm of searchHistory) {
+        const searchTermElement = $('<li>');
+        searchTermElement.text(searchTerm);
+    }
+}
