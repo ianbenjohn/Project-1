5b0bae91a695acad3d6c5e5147626db0875f6ce5
Max Lemieux <max.lemieux@gmail.com>
2020-03-05 19:05:10 -0800

    print ingredients and nutrition facts from Nutritionix API call

--- assets/js/nutritionix.js
+++ assets/js/nutritionix.js
@@ -1,3 +1,21 @@
+/* Food groups from Nutritionix API
+ https://docs.google.com/document/d/1_q-K-ObMTZvO0qUEAxROrN3bwMujwAN25sLHwJzliK0/edit#bookmark=id.o2dyqyfryvuc
+*/
+const foodGroups = {
+    1: 'dairy',
+    2: 'protein',
+    3: 'fruit',
+    4: 'vegetable',
+    5: 'grain',
+    6: 'fat',
+    7: 'legume',
+    8: '',
+    9: '',
+};
+
+/* Get nutrition facts from Nutritionix API
+    https://docs.google.com/document/d/1_q-K-ObMTZvO0qUEAxROrN3bwMujwAN25sLHwJzliK0/edit
+*/
 const getNutrition = (ingredients) => {
     let ingredientStory = '';
     const queryUrl = 'https://trackapi.nutritionix.com/v2/natural/nutrients';
@@ -28,6 +46,57 @@ const getNutrition = (ingredients) => {
         ),
     }
     $.ajax(settings).done(function(response) {
-        console.log(response);
+        /* Call function to print the ingredients and nutrition from the response */
+        printIngredients(response);
     });
+};
+
+/* Display ingredients on page */
+const printIngredients = (ingredientResponse) => {
+    const nutritionBodyEl = $('#nutrition_body');
+    nutritionBodyEl.empty();
+
+    /* Define total amounts to be added up in the loop */
+    let totalCalories = 0;
+    const totalCaloriesEl = $('#total_cal');
+    let totalWeight = 0;
+    const totalWeightEl = $('#total_weight');
+
+    /* Loop over ingredients in response and print them */
+    for (let i=0; i<ingredientResponse.foods.length; i++) {
+        const nutritionIngredientRow = $('<tr>');
+
+        /* Quantity */
+        const quantityData = $('<td>').text(ingredientResponse.foods[i].serving_qty);
+        nutritionIngredientRow.append(quantityData);
+        const quantityType = $('<td>').text(ingredientResponse.foods[i].serving_unit);
+        nutritionIngredientRow.append(quantityType);
+
+        /* Ingredient name */
+        const foodName = $('<td>').text(ingredientResponse.foods[i].food_name);
+        nutritionIngredientRow.append(foodName);
+
+        /* Calories */
+        const thisCalories = ingredientResponse.foods[i].nf_calories;
+        totalCalories += thisCalories;
+        const caloriesData = $('<td>').text(thisCalories);
+        nutritionIngredientRow.append(caloriesData);
+
+        /* Weight */
+        const thisWeight = ingredientResponse.foods[i].serving_weight_grams;
+        totalWeight += thisWeight;
+        const weightData = $('<td>').text(thisWeight);
+        nutritionIngredientRow.append(weightData);
+
+        /* Get the food group from our object using the tag data */
+        const foodGroup = foodGroups[ingredientResponse.foods[i].tags.food_group]
+        const foodGroupEl = $('<td>').text(foodGroup);
+        nutritionIngredientRow.append(foodGroupEl);
+        console.log(nutritionIngredientRow);
+        nutritionBodyEl.append(nutritionIngredientRow); 
+    };
+
+    /* Update totals on page */
+    totalCaloriesEl.text(`${totalCalories.toFixed(0)} cal`);
+    totalWeightEl.text(`${totalWeight.toFixed(2)} g`);
 };
